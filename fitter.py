b7625e1489c7f2bf2618757ec552189bec431900
Alex Carlin <carlin@ucdavis.edu>
2015-08-16 17:17:13 -0700

    Cleaned up even more

--- fitter.py
+++ fitter.py
@@ -5,10 +5,10 @@ import pandas
 import os 
 import random
 import StringIO
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory, make_response
+from flask import Flask, request #redirect, url_for, send_from_directory, make_response
 from numpy import diag, sqrt  
 from scipy.optimize import curve_fit
-from werkzeug import secure_filename
+#from werkzeug import secure_filename
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import matplotlib.pyplot as plt
 
@@ -33,7 +33,6 @@ def fit( df ):
   return pandas.Series( { 
     'kcat': '%.0f ± %.0f' % (popt[0], perr[0]), 
     'km': '%.4f ± %.4f' % (popt[1], perr[1]),
-    'plot': plt.subplots()
     #'ki': '%.4f ± %.4f' % (popt[2], perr[2]),
   } )
   
@@ -44,29 +43,15 @@ def upload_file():
     df = pandas.read_csv( file ) 
     grouped = df.groupby( by='sample' ).apply( fit ) 
     return '<pre>' + str( grouped ) + '</pre>'
-  #return render_template( 'index.html' ) 
 
   return '''
     <!doctype html>
     <title>fitter.py</title>
-    <style>
-      div { 
-        font: normal 18px 'Source Serif Pro'; 
-        background-color: #eee; 
-        width: 400px; 
-        padding: 3em; 
-        margin: 200px auto; 
-      }  
-      form input { font-size: 18px; } 
-      #file { width: 200px; height: 200px; background-color: #222; } 
-    </style>
-    <div>
     <h1>Upload your data</h1>
     <form action="" method=post enctype=multipart/form-data>
       <input type=file name=file id=file>
       <input type=submit value=Fit>
     </form>
-    </div>
     '''
 
 if __name__ == '__main__':
